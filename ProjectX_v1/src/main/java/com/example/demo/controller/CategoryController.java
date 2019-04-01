c8dfef1bcc9bebf9e3367c9ae7851de02025dc4f
Phuong <phuonglt101195@gmail.com>
2019-04-01 23:33:11 +0700

    Cross fix

--- ProjectX_v1/src/main/java/com/example/demo/controller/CategoryController.java
+++ ProjectX_v1/src/main/java/com/example/demo/controller/CategoryController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import com.example.demo.entities.Category;
 import com.example.demo.service.CategoryService;
 
 @Controller
+@CrossOrigin(origins = { "*" })
 public class CategoryController {
 
 	private CategoryService categoryService;
